07735d0242cc12263ae09c3c9ef73a2f048f2b0f
Faizan Ansari <39953141+5zan-Code@users.noreply.github.com>
2024-01-02 15:08:33 +0000

    Created JWT tokens and password encryption

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
+import colors from "colors"
 
+dotenv.config()
 
 const connectDB = async ()=>{
     try {
-        const connectionInstance = await mongoose.connect(db_url)
-        console.log(`Database connected !! DB Host: ${connectionInstance} `)    
+        const connectionInstance = await mongoose.connect(process.env.DB_URL)
+        console.log(`Database connected !! DB Host: ${connectionInstance.connection.host} `.bgGreen.black)    
     } 
     catch (error) {
         console.log("MongoDB Connection error", error)
