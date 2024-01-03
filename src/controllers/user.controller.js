7e1828ddc69510b93a810e66313968395d5be911
Faizan Ansari <39953141+5zan-Code@users.noreply.github.com>
2024-01-03 03:23:20 +0000

    Modified user controller for testing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,25 @@
 import { asyncHandler } from '../utils/asyncHandler.js'
 
 const registerUser = asyncHandler( async (req,res) => {
-    res.status(200).json({
-        message: "Ok"
-    })
+    // get user details from frontend
+    const {username, fullname, email, password} = req.body
+    console.log("Email: " , email, "Fullname:", fullname)
+    // Validation - not empty
+
+    // Check if user already exists: username, email
+
+    // check for images, check for avator?
+
+    // upload them to cloudinary, avatar
+    
+    // create user object - create entry in DB
+
+    // remove password and refresh token field from response
+
+    // Check for user creation 
+
+    // Return response
+
 })
 
 
